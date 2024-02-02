362c6baa6229684602345c7d364742a6faca0e89
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2024-02-01 21:12:32 -0800

    added code to prevent creation of new Audio for very trigger

--- src/components/Render/Music.js
+++ src/components/Render/Music.js
@@ -1,15 +1,20 @@
 import useEffectSkipMount from "@/hooks/useEffectSkipMount";
+import React from "react";
 
 function Music({ index, instrument, trigger }) {
+  const audio = React.useRef();
+
+  React.useEffect(() => {
+    if (typeof Audio !== "undefined") {
+      audio.current = new Audio(`/${instrument}/${(index % 8) + 1}.mp3`);
+    }
+  }, [instrument, index]);
+
   useEffectSkipMount(() => {
-    if (trigger) {
-      const audio = new Audio(`/${instrument}/${(index % 8) + 1}.mp3`);
-      audio.volume = 0.1;
-      audio.play();
-      // audio.play().catch(error => {
-      //   // Auto-play was blocked.
-      //   console.log('Auto-play Error: ', error);
-      // });
+    if (trigger && audio.current) {
+      audio.current.currentTime = 0;
+      audio.current.volume = 0.1;
+      audio.current.play();
     }
   }, [trigger, instrument, index]);
 
