7ef5a49dcfaf1944f133f9172bf29f42f03295b2
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2024-02-01 17:40:28 -0800

    customized radix components for the form

---
+++ src/components/Form/Slider.js
@@ -0,0 +1,50 @@
+import React from "react";
+import * as RadixSlider from "@radix-ui/react-slider";
+import * as RadixLabel from "@radix-ui/react-label";
+import clsx from "clsx";
+import useValueMap from "@/hooks/useValueMap";
+
+function Slider({
+  label,
+  min,
+  max,
+  pseudoMin = 0,
+  pseudoMax = 100,
+  step = 1,
+  value,
+  setValue,
+  classes = [],
+}) {
+  return (
+    <div
+      className={clsx(
+        "w-full flex flex-col flex-wrap items-start justify-center gap-1 pb-4 pt-2",
+        classes
+      )}
+    >
+      <RadixLabel.Root className="text-[15px] font-medium leading-[35px] text-white">
+        {label}: {useValueMap(value, min, max, pseudoMin, pseudoMax)}
+      </RadixLabel.Root>
+      <RadixSlider.Root
+        name={label.split(" ").join("-").toLowerCase()}
+        className="relative flex items-center select-none touch-none w-full h-5 cursor-pointer"
+        min={min}
+        max={max}
+        step={step}
+        value={[value]}
+        onValueChange={setValue}
+        minStepsBetweenThumbs={step}
+      >
+        <RadixSlider.Track className="bg-neutral-700 relative grow rounded-full h-[3px]">
+          <RadixSlider.Range className="absolute bg-neutral-50 rounded-full h-full" />
+        </RadixSlider.Track>
+        <RadixSlider.Thumb
+          className="block w-5 h-5 bg-neutral-50 shadow-[0_2px_10px] shadow-blackA4 rounded-[10px] hover:bg-violet3 focus:outline-none focus:shadow-[0_0_0_5px] focus:shadow-neutral-500"
+          aria-label={label}
+        />
+      </RadixSlider.Root>
+    </div>
+  );
+}
+
+export default Slider;
---
+++ src/components/Form/Switch.js
@@ -0,0 +1,35 @@
+import React from "react";
+import * as RadixSwitch from "@radix-ui/react-switch";
+import * as RadixLabel from "@radix-ui/react-label";
+import clsx from "clsx";
+
+function Switch({ label, value, setValue, classes = [] }) {
+  const switchId = React.useId();
+
+  return (
+    <div
+      className={clsx(
+        "w-full flex flex-row items-center justify-start gap-2 pb-4",
+        classes
+      )}
+    >
+      <RadixLabel.Root
+        className="text-[15px] font-medium leading-[35px] text-white"
+        htmlFor={switchId}
+      >
+        {label}
+      </RadixLabel.Root>
+      <RadixSwitch.Root
+        id={switchId}
+        name={label.split(" ").join("-").toLowerCase()}
+        className="w-[42px] h-[25px] bg-transparent ring-2 ring-inset ring-neutral-700 rounded-full relative shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-neutral-500 data-[state=checked]:bg-neutral-700 outline-none cursor-pointer"
+        checked={value}
+        onCheckedChange={setValue}
+      >
+        <RadixSwitch.Thumb className="block w-[21px] h-[21px] bg-white rounded-full transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]" />
+      </RadixSwitch.Root>
+    </div>
+  );
+}
+
+export default Switch;
