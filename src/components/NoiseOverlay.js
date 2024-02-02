40a28eefb60e6f2284c6be1400580786648a2912
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2024-02-01 17:42:24 -0800

    noise overlay over the app for a fancier look

---
+++ src/components/NoiseOverlay.js
@@ -0,0 +1,10 @@
+function NoiseOverlay() {
+  return (
+    <div
+      id="noise-overlay"
+      className="fixed top-0 left-0 h-full w-full pointer-events-none opacity-55"
+    />
+  );
+}
+
+export default NoiseOverlay;
