1857015be076938bb4c02e60e3fa9b31f75225df
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2024-03-26 19:33:50 -0700

    added new media query class for screen wider than 1920px

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+        "3xl": "1920px",
+      },
     },
   },
   plugins: [],
